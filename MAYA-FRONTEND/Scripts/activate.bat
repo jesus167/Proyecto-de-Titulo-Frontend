8a9e8b105b07691aa4d85fa338ab344e86eac11c
orlando167 <ogutierrespinosa@gmail.com>
2023-04-22 13:36:14 -0400

    Corrección nombre proyecto

--- MAYA-FRONTEND/Scripts/activate.bat
+++ MAYA-FRONTEND/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jesus\OneDrive\Escritorio\Proyecto de Titulo Frontend\MAYA-FRONTEND
+set VIRTUAL_ENV=C:\Proyecto de Titulo Frontend\Proyecto-de-Titulo-Frontend\MAYA-FRONTEND
 
 if not defined PROMPT set PROMPT=$P$G
 
