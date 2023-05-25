be5e1ff89dd966e506e716aef6528efab4116645
orlando167 <ogutierrespinosa@gmail.com>
2023-05-24 20:03:45 -0400

    commit para pasar a producción

--- MAYA/Clientas/views.py
+++ MAYA/Clientas/views.py
@@ -7,74 +7,69 @@ url_region= "http://127.0.0.1:8000/api/region/"
 url_comuna= "http://127.0.0.1:8000/api/comuna/"
 
 
-def paises(request):
+def clientas(request):
+    return render(request, 'Clientas/clientas.html')
+
+def registro_clientas(request):
+
     respuesta = requests.get('http://127.0.0.1:8000/api/pais/')
     if respuesta.status_code == 200:
         paises = respuesta.json()
     else:
         print('Error en la búsqueda')
-    pais = {"paises":paises}
-    return render(request,'Clientas/registro_clientas.html', pais)
+    
 
-def region(request):
     respuesta = requests.get('http://127.0.0.1:8000/api/region/')
     if respuesta.status_code == 200:
         region = respuesta.json()
     else:
         print('Error en la búsqueda')
-    regiones = {"paises":region}
-    return render(request,'Clientas/registro_clientas.html', regiones)
+    
 
-def comuna(request):
     respuesta = requests.get('http://127.0.0.1:8000/api/comuna/')
     if respuesta.status_code == 200:
         comunas = respuesta.json()
     else:
         print('Error en la búsqueda')
-    comuna = {"paises":comunas}
-    return render(request,'Clientas/registro_clientas.html', comuna)
-
-def clientas(request):
-    return render (request, 'Clientas/clientas.html')
-
-
-def registro_clientas(request):
-
-
+    context = {"comunas":comunas,
+               "region": region,
+               "paises": paises}
 
 
     if request.method == 'POST':
         form = form(request.POST)
-        if form.is_valid():
-            registro = {
+        registro = {
                 "rut_clienta": request.POST['rut'],
-                "dv_clienta":request.POST['rut'], 
-                "tipo_clienta":request.POST['rut'],
-                "nombre":request.POST['rut'], 
-                "apellido":request.POST['rut'], 
-                "nom_fantasia":request.POST['rut'],
-                "rep_legal_cod_rep_legal":request.POST['rut'],
-                "correo":request.POST['rut'],
-                "tel": request.POST['rut'],
+                "dv_clienta":request.POST['DV'], 
+                "tipo_clienta":request.POST['tipo_clienta'],
+                "nombre":request.POST['nombre'], 
+                "apellido":request.POST['apellido'], 
+                "nom_fantasia":request.POST['nombre_fantasia'],
+                "rep_legal_cod_rep_legal":request.POST['representante'],
+                "correo":request.POST['correo'],
+                "tel": request.POST['telefono'],
                 "pais_cod_pais": request.POST['rut'],
                 "region_cod_pais":request.POST['rut'],
                 "comuna_cod_comuna":request.POST['rut'],
-                "calle":request.POST['rut'],
-                "numero":request.POST['rut'],
-                "complemento":request.POST['rut'], 
-                "giro":request.POST['rut'] 
+                "calle":request.POST['direccion'],
+                "numero":request.POST['numero'],
+                "complemento":request.POST['Complemento'], 
+                "giro":request.POST['giro'] 
             }
+        response  = requests.post(url_clienta, registro)
+        if response.status_code == 200:
+            print('Clienta registrada')
 
 
-    return render (request, 'Clientas/registro_clientas.html')
+    return render (request, 'Clientas/registro_clientas.html', context)
 
 def listar_clientas(request):
     data =  requests.get(url_clienta)
 
     if data.status_code == 200:
-        data.json()
-    context = data
+        clientas = data.json()
+    context = {'clientas': clientas}
 
-    return render (request, 'Clientas/listar_clientas.html', {'context': context})
+    return render (request, 'Clientas/listar_clientas.html', context)
 
 
